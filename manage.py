2b9045bd4acfb85d8261ee2f61b37e12b15892fd
Terrile <minzou@live.com>
2015-06-11 17:36:21 +0800

    update data loading

--- manage.py
+++ manage.py
@@ -2,8 +2,11 @@
 import os
 import sys
 import graphviz_web.settings
+loaded = False
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "graphviz_web.settings")
-    graphviz_web.settings.graph.load('graph.txt')
+    if loaded==False:
+        graphviz_web.settings.graph.load('graph.txt')
+        loaded = True
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
