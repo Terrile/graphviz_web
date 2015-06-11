aa8168ed7d460b18161eef241ee620817898891a
Terrile <minzou@live.com>
2015-06-11 17:25:20 +0800

    add template

--- manage.py
+++ manage.py
@@ -4,6 +4,6 @@ import sys
 import graphviz_web.settings
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "graphviz_web.settings")
-    graphviz_web.settings.graph.load('tiny.txt')
+    graphviz_web.settings.graph.load('graph.txt')
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
