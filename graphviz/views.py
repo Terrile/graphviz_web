4e48e5318b7d38d7635abe7847a76444e6871240
Terrile <minzou@live.com>
2015-06-11 16:43:17 +0800

    add template

--- graphviz/views.py
+++ graphviz/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import Template,Context
 import graphviz_web.settings
+from django.template.loader import get_template
 import manage
 def index(request):
     return HttpResponse(u'Hello World')
@@ -11,9 +12,19 @@ def get_video_result(request):
     key1 = request.GET.get('lquery')
     key2 = request.GET.get('rquery')
     print key1, key2
-    res_list = graphviz_web.settings.graph.get_query_connection(key1,key2)
+    query_list = graphviz_web.settings.graph.get_query_connection(key1,key2)
     content = 'Insight<br/>'
-    for res in res_list:
-        content += res[0].encode('utf-8')+'<br/>'
-    return HttpResponse(content)
+    query_res_list = []
+    for res in query_list:
+        #content += res[0].encode('utf-8')+'<br/>'
+        query_res_list.append(res[0].encode('utf-8'))
+    video_list = graphviz_web.settings.graph.get_video_connection(key1,key2)
+    video_res_list = []
+    for res in video_list:
+        video_res_list.append(res[0].encode('utf-8'))
+    template = get_template('insight.html')
+    html = template.render(Context({'videolist':video_res_list,'querylist':query_res_list}))
+    return HttpResponse(html)
+#    return HttpResponse(content)
+
 # Create your views here.
